28e3be9172aac661957041b0c0acb29b427be630
Alpha Hinex <AlphaHinex@gmail.com>
2023-03-25 19:46:58 +0800

    Before homework

---
+++ 70/js/button.js
@@ -0,0 +1,142 @@
+//添加头像
+
+//创建数组存储本周免费
+
+//创建数组存储新手推荐
+
+//创建数组存储坦克
+
+//创建数组存储战士
+
+//创建数组存储刺客
+
+//创建数组存储法师
+
+//创建数组存储射手
+
+//创建数组存储辅助
+
+//将对应角色存入数组中
+
+//为单选框添加点击事件
+
+//创建方法，可以将传入的数组显示
+
+//创建方法让对应按钮点击时只显示相应英雄
+
+
+//add()方法预留
+function add() {
+	var num = 0;
+	arrHead[num].innerHTML = "孙策";
+	tank[tank.length] = arrHead[num];
+	soldiers[soldiers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "梦琪";
+	tank[tank.length] = arrHead[num];
+	free[free.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "苏烈";
+	tank[tank.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "凯";
+	tank[tank.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "李信";
+	soldiers[soldiers.length] = arrHead[num];
+	assassins[assassins.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "狂铁";
+	soldiers[soldiers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "裴擒虎";
+	soldiers[soldiers.length] = arrHead[num];
+	free[free.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "上官婉儿";
+	free[free.length] = arrHead[num];
+	assassins[assassins.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "司马懿";
+	assassins[assassins.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "元歌";
+	free[free.length] = arrHead[num];
+	assassins[assassins.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "女娲";
+	mages[mages.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "安琪拉";
+	mages[mages.length] = arrHead[num];
+	free[free.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "王昭君";
+	mages[mages.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "妲己";
+	mages[mages.length] = arrHead[num];
+	introduction[introduction.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "伽罗";
+	archers[archers.length] = arrHead[num];
+	free[free.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "百里守约";
+	archers[archers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "公孙离";
+	archers[archers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "黄忠";
+	archers[archers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "盾山";
+	AUXs[AUXs.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "明世隐";
+	AUXs[AUXs.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "鬼谷子";
+	AUXs[AUXs.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "大桥";
+	AUXs[AUXs.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "亚瑟";
+	soldiers[soldiers.length] = arrHead[num];
+	introduction[introduction.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "狄仁杰";
+	introduction[introduction.length] = arrHead[num];
+	archers[archers.length] = arrHead[num];
+	num++;
+
+	arrHead[num].innerHTML = "鲁班";
+	introduction[introduction.length] = arrHead[num];
+	archers[archers.length] = arrHead[num];
+	num++;
+}
